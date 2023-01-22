2880bd6ce9d146d3a8d9dca1de7043701b20ec90
shibuwodai404 <112820035+shibuwodai404@users.noreply.github.com>
2023-01-22 23:16:15 +0800

    Add files via upload

---
+++ forrddatafolder.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import glob
+import os
+
+os.chdir("/Users/leogy/study/HKUStudy/Dissertation/DataDownload/MILimbEEG/M7/test")  #要拼接的文件夹及其完整路径，注意不要包含中文
+
+
+csv_list = glob.glob('*.csv')
+print('发现%s个CSV文件'% len(csv_list))
+print('对文件进行处理中')
+for i in csv_list:
+    fr = open(i,'r',encoding='utf-8').read()
+    with open('M7test.csv','a',encoding='utf-8') as f:
+        f.write(fr)
+print('所有文件合并完成！')    #完成后在这个路径的合并文件需要删除第一列才能识别16channels
